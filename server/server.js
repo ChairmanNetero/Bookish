7cbd87a1cc82b7ba53d8da28eb61e6e1da5b84e0
Netero <m_benab@live.concordia.ca>
2025-08-06 23:38:38 -0400

    Create first endpoint method

--- server/server.js
+++ server/server.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Server started on port ${port}`)})
+// Create first endpoint
+app.get('/', (req, res) => {
+    console.log("Welcome!", req.method);
+    res.sendStatus(201);
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
