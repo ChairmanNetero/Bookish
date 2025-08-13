6e8b450168be9a927d46429edc1142e37851b711
Netero <m_benab@live.concordia.ca>
2025-08-12 23:44:54 -0400

    Add Login route in the authRoutes file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -51,6 +51,48 @@ router.post('/SignUp', async (req, res) => {
     }
 })
 
+// Login route
+router.post('/Login', async (req, res) => {
+    try {
+        const {email, password} = req.body
 
+        // Verify if input is not empty
+        if (!email || !password) {
+            return res.status(400).json({error: 'Email and password is required'})
+        }
+
+        // Find user by email
+        const user = await prisma.user.findUnique({where: {email}})
+        if (!user) {
+            return res.status(400).json({error: 'User does not exist'})
+        }
+
+        // Verify password
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword) {
+            return res.status(400).json({error: 'Invalid email or password'})
+        }
+
+        // Generate JWT token
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '24h'}
+        )
+
+        res.status(200).json({
+            message: 'User logged in successfully',
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+            }
+        })
+
+    } catch (error) {
+        console.error('Registration error:', error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+})
 
 export default router
